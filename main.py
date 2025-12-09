c11d4c954012119f2d23379a023673ff48ae8b60
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-12-08 19:05:08 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Write a program to return the country code for various countries. 
+# Here’s a dictionary of different country codes - 
+# {'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}.
+b={'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+for key,value in b.items():
+    print(value)
